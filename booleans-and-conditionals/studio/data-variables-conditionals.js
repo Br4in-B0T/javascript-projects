214cf5af269bbb61f362d2a57f988c8ed80528d0
Br4in-B0T <Thecbrainerd@gmail.com>
2024-06-19 09:37:14 -0500

    I hope it's okay that I used 1 statement- Complete

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -1,7 +1,39 @@
 // Initialize Variables below
-
-// add logic below to verify total number of astronauts for shuttle launch does not exceed 7
-
+let date = "Monday 2019-03-18";
+let time = "10:05:34 AM";
+let astronautCount = 7;
+let astronautStatus = "ready";
+let averageAstronautMassKg = 80.7;
+let crewMassKg = astronautCount * averageAstronautMassKg ;
+let fuelMassKg = 760000;
+let shuttleMassKg = 74842.31;
+let totalMassKg = crewMassKg + fuelMassKg +shuttleMassKg;
+let maximumMassLimit = 850000;
+let fuelTempCelsius = -225;
+let minimumFuelTemp = -300;
+let maximumFuelTemp =  -150;
+let fuelLevel = "100%";
+let weatherStatus = "clear";
+let preparedForLiftOff = true;
+let kg = " kg"
+// add logic below 
+if (astronautCount <= 7 && astronautStatus === "ready" && totalMassKg < 850000 && fuelLevel === "100%" && weatherStatus === "clear" && fuelTempCelsius >= -300 && fuelTempCelsius <= -150){
+    console.log("All systems are a go! Initiating space shuttle launch sequence.")
+    console.log("----------------------------------------------------------------")
+    console.log("Date: " + date)
+    console.log("Time: " + time)
+    console.log("Astronaut Count: " + astronautCount)
+    console.log("Crew Mass: " + crewMassKg + kg)
+    console.log("Fuel Mass: " + fuelMassKg + kg)
+    console.log("Shuttle Mass: " + shuttleMassKg + kg)
+    console.log("Total Mass: " + totalMassKg + kg)
+    console.log("Fuel Temperature: " + fuelTempCelsius + " ℃")
+    console.log("Weather Status: " + weatherStatus)
+    console.log("----------------------------------------------------------------")
+    console.log("Have a safe trip astronauts!")
+} else {
+    console.log("Systems Status Error! Check launch status requirments.")
+}
 // add logic below to verify all astronauts are ready
 
 // add logic below to verify the total mass does not exceed the maximum limit of 850000
